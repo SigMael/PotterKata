ef37b67d219369388648c160b558ed050267665d
M22700 <mael.saigaroudi@ext.bpifrance.fr>
2019-11-21 14:12:35 +0100

    Version Ok pour un set de livre max

--- src/main/java/Potter.java
+++ src/main/java/Potter.java
@@ -1,31 +1,101 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 
 public class Potter {
-    public double processPrice(double[] prices) {
-        List<Double> books = DoubleStream.of(prices).boxed().collect(Collectors.toList());
-        if (books.size() == 0)
-            return 0;
-
+	private final int[] BOOK_NUMBERS = {0, 1, 2, 3, 4};
+	private final double BOOK_STANDARD_PRICE = 8;
+	private final double REDUCTION_LVL_1 =  BOOK_STANDARD_PRICE * 2 * 0.95;
+	private final double REDUCTION_LVL_2 =  BOOK_STANDARD_PRICE * 3 * 0.9;
+	private final double REDUCTION_LVL_3 =  BOOK_STANDARD_PRICE * 4 * 0.8;
+	private final double REDUCTION_LVL_4 =  BOOK_STANDARD_PRICE * 5 * 0.75;
+	
+	private final double[] reduction = {
+			REDUCTION_LVL_1,
+			REDUCTION_LVL_2,
+			REDUCTION_LVL_3,
+			REDUCTION_LVL_4
+	};
+	
+	private ArrayList<Integer> occurences;
+    public double processPrice(int[] prices) {
+        List<Integer> books = IntStream.of(prices).boxed().collect(Collectors.toList());
+        if (books.isEmpty())
+        	return 0;
+                
         if(books.size() == 1){
-            return 8 * books.size();
+            return BOOK_STANDARD_PRICE * books.size();
         }
 
-        if(books.size() == 2){
-            if(books.contains(books.remove(0)))
-                return 8 * 2;
-            return 8 * 2 * 0.95;
-        }
-
-        if(books.size() == 3){
-            if(books.get(0).equals(books.get(1)) && books.get(1).equals(books.get(2)))
-                return 8*3;
-            return 8 * 3 * 0.9;
-        }
-
-        return 0;
+        readOccurences(books);
+        
+        int lesserOccurence = getLesserOccurence(books);
+        System.out.println(lesserOccurence);
+        int bookInSet = getBookSet(lesserOccurence);
+        System.out.println("setOfBook="+bookInSet);
+        return calculFinalPrice(books, bookInSet);
     }
+
+	private double calculFinalPrice(List<Integer> books, int bookInSet) {
+		double finalReduction =0;
+        if(bookInSet > 0)
+        	finalReduction = reduction[bookInSet - 2];
+        double finalPrice = finalReduction + (books.size() - bookInSet) * 8;
+		return finalPrice;
+	}
+
+	private int getBookSet(int lesserOccurence) {
+		int bookInSet = 0;
+        for (int i = 0; i < lesserOccurence; i++) {
+        	bookInSet = 0;
+			if(occurences.get(0) >= i + 1) {
+				occurences.set(0, occurences.get(0) - 1);
+				bookInSet++;
+			}
+			if(occurences.get(1) >= i + 1) {
+				occurences.set(1, occurences.get(1) - 1);
+				bookInSet++;		
+			}
+			if(occurences.get(2) >= i + 1) {
+				occurences.set(2, occurences.get(2) - 1);
+				bookInSet++;
+			}
+			if(occurences.get(3) >= i + 1) {
+				occurences.set(3, occurences.get(3) - 1);
+				bookInSet++;
+			}
+			if(occurences.get(4) >= i + 1) {
+				occurences.set(4, occurences.get(4) - 1);
+				bookInSet++;
+			}
+		}
+		return bookInSet;
+	}
+
+	private int getLesserOccurence(List<Integer> books) {
+		int lesserOccurence = books.size();
+        for (Integer occurence: occurences) {
+        	if(occurence != 0)
+        		lesserOccurence = occurence < lesserOccurence ? occurence : lesserOccurence;
+		}
+        return lesserOccurence;
+	}
+	
+	private void readOccurences(List<Integer> books) {
+		this.occurences = new ArrayList<>();
+		
+		for (int i = 0; i < BOOK_NUMBERS.length; i++) {
+			this.occurences.add(getOccurence(books, BOOK_NUMBERS[i]));
+		}
+	}
+
+	private int getOccurence(List<Integer> books, int val) {
+		int occurenceBook = Collections.frequency(books, val);
+		return occurenceBook;
+	}
 
 }
---
+++ src/test/java/PotterProcessPriceShould.java
@@ -0,0 +1,63 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PotterProcessPriceShould {
+
+    private Potter potter;
+
+    @Before
+    public void initTest(){
+        potter = new Potter();
+    }
+
+    @Test
+    public void return_0_when_no_book_bought(){
+        Assert.assertEquals(0, potter.processPrice(new int[] {}), 0);
+    }
+
+    @Test
+    public void return_8_when_one_book_is_bought(){
+        Assert.assertEquals(8, potter.processPrice(new int[] {0}), 0);
+    }
+
+    @Test
+    public void return_expected_reduction_when_two_books_are_bought(){
+        Assert.assertEquals(8 * 2 * 0.95, potter.processPrice(new int[] {0, 1}), 0);
+    }
+
+    @Test
+    public void return_no_reduction_when_two_identical_books_are_bought(){
+        Assert.assertEquals(8 * 2 , potter.processPrice(new int[] {0, 0}), 0);
+    }
+
+    @Test
+    public void return_expected_when_three_different_books_are_bought(){
+        Assert.assertEquals(8 * 3 * 0.9 , potter.processPrice(new int[] {0, 1, 2}), 0);
+    }
+
+    @Test
+    public void return_no_reduction_when_three_identical_books_are_bought(){
+        Assert.assertEquals(8 * 3 , potter.processPrice(new int[] {2, 2, 2}), 0);
+    }
+
+    @Test
+    public void return_lesser_reduction_when_2_identical_books_and_1_different_are_bought(){
+        Assert.assertEquals(8 * 2 * 0.95 + 8 , potter.processPrice(new int[] {1, 2, 1}), 0);
+    }
+    
+    @Test
+    public void return_expected_reduction_when_all_5_different_books_are_bought(){
+        Assert.assertEquals(8 * 5 * 0.75 , potter.processPrice(new int[] {0, 1, 2, 3, 4}), 0);
+    }
+    
+    @Test
+    public void return_expected_price_when_4_different_books_and_another_identical_are_bought(){
+        Assert.assertEquals(8 * 4 * 0.80 + 8, potter.processPrice(new int[] {0, 1, 2, 3, 3}), 0);
+    }
+    
+    @Test
+    public void return_expected_price_when_2_sets_of_2_books_are_bought(){
+        Assert.assertEquals( (8 * 2 * 0.9)*2, potter.processPrice(new int[] {0, 1, 0, 1}), 0);
+    }
+}
